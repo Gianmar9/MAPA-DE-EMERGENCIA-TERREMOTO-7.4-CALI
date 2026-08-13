933a0ccedbfcd592f7e6618018464f949d1276ba
Gianmar9 <marloncifuentesgiron@gmail.com>
2026-08-13 12:17:25 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.SERVIDOR_MAPA = "https://mapa-de-emergencia-cali.marloncifuentesgiron.workers.dev";
+window.SERVIDOR_MAPA = "https://mapa-de-emergencia-cali.marloncifuentesgiron.workers.dev/";
